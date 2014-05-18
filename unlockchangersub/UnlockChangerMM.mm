d84bc685b42d33e91bfd9ef857671645da667724
mftempelke <mftemp@gmail.com>
2014-05-18 19:31:46 +0200

    Update UnlockChangerMM.mm
    Weer terug naar unlocker

--- unlockchangersub/UnlockChangerMM.mm
+++ unlockchangersub/UnlockChangerMM.mm
@@ -7,8 +7,7 @@
 @implementation UnlockChangerListController
 - (id)specifiers {
 	if(_specifiers == nil) {
-		// werkt ook rarara ( maar iets ander weer niet _specifiers = [[self loadSpecifiersFromPlistName:@"UnlockChangerMFOKSUB" target:self] retain];
-		_specifiers = [[self loadSpecifiersFromPlistName:@"UnlockChangerMFOKSUB" target:self] retain];
+		_specifiers = [[self loadSpecifiersFromPlistName:@"UnlockChanger" target:self] retain];
 	}
 	return _specifiers;
 }
